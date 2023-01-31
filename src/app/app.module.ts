8e75acbf90b56f22d8cc8e1d0d67a0a1f8a1cfc7
Deiby2020 <64172570+Deiby2020@users.noreply.github.com>
2023-01-31 18:40:29 -0400

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
+import { LayoutsModule } from './layouts/layouts.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    LayoutsModule,
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
